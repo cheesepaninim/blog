33222da8bdabfca1f3bfa2ebeefa36c7a0322985
cheesepaninim <hpw0310@gmail.com>
2022-06-28 00:47:49 +0900

    edit Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
                 </div>
                 <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
                     <Link href="/">
-                        <i>{siteMetadata.title}</i>
+                        <i>{siteMetadata.headerTitle}</i>
                     </Link>
                     <div>
                         <i>{` • `}</i>
