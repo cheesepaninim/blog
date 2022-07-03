1551270bd6951b3c8365c46eeca4d05dd57551ca
cheesepaninim <hpw0310@gmail.com>
2022-07-03 19:17:41 +0900

    getAllCategories

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,4 @@
-import { getAllFilesFrontMatter } from '@/lib/mdx'
+import { getAllCategories, getAllFilesFrontMatter } from '@/lib/mdx'
 import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
 import { PageSEO } from '@/components/SEO'
@@ -7,22 +7,26 @@ export const POSTS_PER_PAGE = 10
 
 export async function getStaticProps() {
     const posts = await getAllFilesFrontMatter('blog')
+    const categories = await getAllCategories('blog')
     const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
     const pagination = {
         currentPage: 1,
         totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
     }
 
-    return { props: { initialDisplayPosts, posts, pagination } }
+    return { props: { initialDisplayPosts, posts, pagination, categories } }
 }
 
-export default function Blog({ posts, initialDisplayPosts, pagination }) {
+export default function Blog({ posts, initialDisplayPosts, pagination, categories }) {
+    console.log(categories)
+
     return (
         <>
             <PageSEO
                 title={`Blog - ${siteMetadata.author}`}
                 description={siteMetadata.description}
             />
+
             <ListLayout
                 posts={posts}
                 initialDisplayPosts={initialDisplayPosts}
