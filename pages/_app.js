71a400f7edd8f7f6e7656da61254ef8f6cbb3bea
cheesepaninim <hpw0310@gmail.com>
2022-07-08 22:15:33 +0900

    ga test

--- pages/_app.js
+++ pages/_app.js
@@ -14,11 +14,14 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import { logEvent } from '@/components/analytics/GoogleAnalytics'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
+    logEvent()
+
     return (
         <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
             <Head>
