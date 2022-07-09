24add5f910878541095955ba7abb4f503f70eff1
cheesepaninim <hpw0310@gmail.com>
2022-07-10 02:54:11 +0900

    async-series.mdx

--- pages/_app.js
+++ pages/_app.js
@@ -22,9 +22,75 @@ import { logEvent } from '@/components/analytics/GoogleAnalytics'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
+// import waterfall from "async-waterfall"
+// import series from "async-series"
+// import series from "async/series";
+
 export default function App({ Component, pageProps }) {
     const r = useRouter()
     useEffect(() => {
+        // const getOrderTime = 100 * 1
+        // const makeFoodTime = 100 * 5
+        // const deliverFoodTime = 100 * 2
+        //
+        // const getOrder = (callback, food) => {
+        //     setTimeout(() => {
+        //         console.log(`${food} ordered!`)
+        //         callback(null, `deilicous ${food}`)
+        //     }, getOrderTime)
+        // }
+        //
+        // const makeFood = (food, callback) => {
+        //     setTimeout(() => {
+        //         console.log(`${food} cooked!`)
+        //         callback(null, `cooked ${food}`)
+        //     }, makeFoodTime)
+        // }
+        //
+        // const deliverFood = (food, callback) => {
+        //     setTimeout(() => {
+        //         console.log(`${food} delivered!`)
+        //         callback(null, 'Delivery The End')
+        //     }, deliverFoodTime)
+        // }
+        //
+        // const order = (food) => {
+        //     waterfall([(callback) => getOrder(callback, food), makeFood, deliverFood], (err, result) => {
+        //         err ? console.log(`err : ${err}`) : console.log(`result : ${result}`)
+        //     })
+        // }
+        //
+        // order('Pizza')
+
+        // const seriesGetOrder = callback => {
+        //     setTimeout(() => {
+        //         console.log(`ordered!`)
+        //         callback(null, 'ordered')
+        //     }, getOrderTime)
+        // }
+        //
+        // const seriesMakeFood = callback => {
+        //     setTimeout(() => {
+        //         console.log(`cooked!`)
+        //         callback(null, 'cooked')
+        //     }, makeFoodTime)
+        // }
+        //
+        // const seriesDeliverFood = callback => {
+        //     setTimeout(() => {
+        //         console.log(`delivered!`)
+        //         callback(null, 'delivered')
+        //     }, deliverFoodTime)
+        // }
+        //
+        // const seriesOrder = () => {
+        //     series([seriesGetOrder, seriesMakeFood, seriesDeliverFood], (err, result) => {
+        //         err ? console.log(`err : ${err}`) : console.log(result)
+        //     })
+        // }
+        //
+        // seriesOrder('Pizza')
+
         logEvent('page_move', `page to: ${r.asPath}`, '', '')
     }, [Component, pageProps])
 
