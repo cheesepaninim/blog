df53f8c561e7cfe26b21271400cdb43af8b129e5
cheesepaninim <hpw0310@gmail.com>
2022-07-11 22:31:27 +0900

    btndown test

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -30,7 +30,7 @@ const siteMetadata = {
         // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
         // Please add your .env file and modify it according to your selection
         // provider: 'buttondown',
-        provider: '',
+        provider: 'buttondown',
     },
     comment: {
         // If you want to use a commenting system other than giscus you have to add it to the
