bf6b7e2d08df1b4201e1388e244d0b62f2a1d323
cheesepaninim <45236936+cheesepaninim@users.noreply.github.com>
2022-07-12 14:09:33 +0900

    Update ListLayout.js

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -60,32 +60,39 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                 <ul>
                     {!filteredBlogPosts.length && 'No posts found.'}
                     {displayPosts.map((frontMatter) => {
-                        const { slug, date, title, summary, tags } = frontMatter
+                        const { slug, date, title, summary, tags, image } = frontMatter
                         return (
                             <li key={slug} className="list-item py-4">
                                 <article
                                     onClick={(_) => linkTo(`/blog/${slug}`)}
                                     className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0"
                                 >
-                                    <dl>
-                                        <dt className="sr-only">Published on</dt>
-                                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                                            <time dateTime={date}>{formatDate(date)}</time>
-                                        </dd>
-                                    </dl>
-                                    <div className="space-y-3 xl:col-span-3">
-                                        <div>
-                                            <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
-                                                {title}
-                                            </h3>
-                                            <div className="flex flex-wrap">
-                                                {tags.map((tag) => (
-                                                    <Tag key={tag} text={tag} />
-                                                ))}
+                                    <div className="grid grid-flow-row-dense grid-cols-4">
+                                        <div className="col-span-3">
+                                            <dl>
+                                                <dt className="sr-only">Published on</dt>
+                                                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                                                    <time dateTime={date}>{formatDate(date)}</time>
+                                                </dd>
+                                            </dl>
+                                            <div className="space-y-3 xl:col-span-3">
+                                                <div>
+                                                    <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                                                        {title}
+                                                    </h3>
+                                                    <div className="flex flex-wrap">
+                                                        {tags.map((tag) => (
+                                                            <Tag key={tag} text={tag} />
+                                                        ))}
+                                                    </div>
+                                                </div>
+                                                <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                                                    {summary}
+                                                </div>
                                             </div>
                                         </div>
-                                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                                            {summary}
+                                        <div>
+                                            <img src={image} alt="thumb image" />
                                         </div>
                                     </div>
                                 </article>
