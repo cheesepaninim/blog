44d5173689957c9ceb1c3ae9bf559e9bd652ac28
cheesepaninim <hpw0310@gmail.com>
2022-07-13 01:23:33 +0900

    .

--- components/Tag.js
+++ components/Tag.js
@@ -16,12 +16,14 @@ const Tag = ({ text }) => {
       </a>
     </Link>*/
 
-        <a
-            onClick={linkTo}
-            className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-        >
-            {text.split(' ').join('-')}
-        </a>
+        <Link href={`/tags/${kebabCase(text)}`}>
+            <a
+                onClick={linkTo}
+                className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            >
+                {text.split(' ').join('-')}
+            </a>
+        </Link>
     )
 }
 
