68236acb87acfc6e7589ccbc3c2a338f51830b2c
cheesepaninim <45236936+cheesepaninim@users.noreply.github.com>
2022-07-14 11:19:29 +0900

    Update MdxImage.js

--- components/MdxImage.js
+++ components/MdxImage.js
@@ -3,11 +3,11 @@ import NextImage from 'next/image'
 // eslint-disable-next-line jsx-a11y/alt-text
 const Image = ({ ...rest }) => (
   <>
-    <p align="center" class="text-slate-200"> • </p>
+    <p align="center" className="text-slate-200"> • </p>
     <div className="img-box">
       <NextImage {...rest} />
     </div>
-    <p align="center" class="text-slate-200"> • </p>
+    <p align="center" className="text-slate-200"> • </p>
   </>
 )
 
